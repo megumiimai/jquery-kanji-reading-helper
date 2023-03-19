9eaec351a2b3e464b7ebecfc2cac127f6fdb4bde
megumiimai <megumi_imai@fout.jp>
2023-03-19 19:32:17 +0900

    kanji reading

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# jquery-reading-helper
+## jquery-kanji-reading-helper
+
+日本語の漢字の読み方がわからない外国人、留学生、子供など便利のプラグインです
+
+### how to
+```
+$(selector).kanji_reading_help();
+
+```
+<img src="https://github.com/megumiimai/jquery-kanji-reading-helper/blob/master/howto/standard.png" width="400">
+
+
+### options: add text color to kanji
+```
+$(selector).kanji_reading_help({
+    addColor: true
+});
+
+```
+<img src="https://github.com/megumiimai/jquery-kanji-reading-helper/blob/master/howto/options.png" width="400">
+
+
--- src/jquery.reading-helper.js
+++ src/jquery.reading-helper.js
@@ -1,24 +1,45 @@
-(function ($) {
-  let textLengthCount = 0;
-  document.addEventListener("DOMContentLoaded", () => {
-    $(".text-color").each((_, element) => {
-      const $textColor = $(element);
-      const text = $textColor.text().trim();
-      $textColor.html("");
-      for (var i = 0; i < text.length; i++) {
-        $textColor.append("<span>" + text[i] + "</span>");
-      }
-      textLengthCount += text.length;
-    });
-  });
+const kuromoji = require("kuromoji");
+const builder = kuromoji.builder({
+  dicPath: "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict/",
+});
+var regexp =
+  /([\u{3005}\u{3007}\u{303b}\u{3400}-\u{9FFF}\u{F900}-\u{FAFF}\u{20000}-\u{2FFFF}][\u{E0100}-\u{E01EF}\u{FE00}-\u{FE02}]?)/mu;
 
-  var s = 0;
-  var tim = setInterval(() => {
-    $(".text-color span").eq(s++).css("color", getColor()).show();
-    if (s == textLengthCount) {
-      clearInterval(tim);
-    }
-  }, 400);
+(function ($) {
+  $.fn.kanji_reading_help = function (options) {
+    this.each(function () {
+      const text = $(this).text().trim();
+      if (!text) return false;
+      $(this).html("");
+      builder.build((error, tokenizer) => {
+        const tokens = tokenizer.tokenize(text);
+        tokens.forEach((token) => {
+          const word = token.surface_form;
+          const isKanji = regexp.test(word);
+          const $s = $("<span>").append(
+            isKanji
+              ? "<ruby>" +
+                  word +
+                  "<rt>" +
+                  kanaToHira(token.reading) +
+                  "</rt></ruby>"
+              : word
+          );
+          if (isKanji && options.addColor) {
+            $s.css("color", getColor());
+          }
+          $(this).append($s);
+        });
+      });
+    });
+  };
+
+  function kanaToHira(str) {
+    return str.replace(/[\u30a1-\u30f6]/g, function (match) {
+      var chr = match.charCodeAt(0) - 0x60;
+      return String.fromCharCode(chr);
+    });
+  }
 
   function getColor() {
     return (
