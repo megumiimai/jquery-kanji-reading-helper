11215fef30c7b48446e24c0988ce7b26fce5e7d2
megumiimai <megumi_imai@fout.jp>
2023-03-24 23:36:06 +0900

    refactoring

--- dict/jquery.reading-helper.js
+++ dict/jquery.reading-helper.js
@@ -9458,59 +9458,70 @@ d=(g[h++]|g[h++]<<8|g[h++]<<16|g[h++]<<24)>>>0;(a.length&4294967295)!==d&&n(Erro
 
 },{}],29:[function(require,module,exports){
 const kuromoji = require("kuromoji");
-const builder = kuromoji.builder({
-  dicPath: "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict/",
-});
-var regexp =
+const REGEXP =
   /([\u{3005}\u{3007}\u{303b}\u{3400}-\u{9FFF}\u{F900}-\u{FAFF}\u{20000}-\u{2FFFF}][\u{E0100}-\u{E01EF}\u{FE00}-\u{FE02}]?)/mu;
 
 (function ($) {
   $.fn.kanji_reading_helper = function (options) {
-    this.each(function () {
-      const text = $(this).text().trim();
-      if (!text) return false;
-      $(this).html("");
-      builder.build((error, tokenizer) => {
-        const tokens = tokenizer.tokenize(text);
-        tokens.forEach((token) => {
-          const word = token.surface_form;
-          const isKanji = regexp.test(word);
-          const $s = $("<span>").append(
-            isKanji
-              ? "<ruby>" +
-                  word +
-                  "<rt>" +
-                  kanaToHira(token.reading) +
-                  "</rt></ruby>"
-              : word
-          );
-          if (isKanji && options.addColor) {
-            $s.css("color", getColor());
-          }
-          $(this).append($s);
-        });
-      });
+    const defaults = {
+      addColor: false,
+    };
+    options = $.extend({}, defaults, options);
+
+    return this.each(async function () {
+      const tokenizer = await tokenizerPromise;
+      replaceText($(this), tokenizer, options);
     });
   };
 
-  function kanaToHira(str) {
-    return str.replace(/[\u30a1-\u30f6]/g, function (match) {
-      var chr = match.charCodeAt(0) - 0x60;
+  const tokenizerPromise = new Promise((resolve, reject) => {
+    kuromoji
+      .builder({ dicPath: "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict/" })
+      .build((err, tokenizer) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(tokenizer);
+        }
+      });
+  });
+
+  const kanaToHira = (str) => {
+    return str.replace(/[\u30a1-\u30f6]/g, (match) => {
+      const chr = match.charCodeAt(0) - 0x60;
       return String.fromCharCode(chr);
     });
-  }
+  };
 
-  function getColor() {
-    return (
-      "rgb(" +
-      Math.floor(Math.random() * 255) +
-      "," +
-      Math.floor(Math.random() * 255) +
-      "," +
-      Math.floor(Math.random() * 255) +
-      ")"
-    );
-  }
+  const randomColor = () => {
+    return `rgb(${Math.floor(Math.random() * 255)},${Math.floor(
+      Math.random() * 255
+    )},${Math.floor(Math.random() * 255)})`;
+  };
+
+  const replaceText = ($element, tokenizer, options) => {
+    const text = $element.text().trim();
+    if (!text) return;
+    $element.html("");
+    const tokens = tokenizer.tokenize(text);
+    for (const token of tokens) {
+      const word = token.surface_form;
+      const isKanji = REGEXP.test(word);
+      const $s = $("<span>").append(
+        isKanji
+          ? "<ruby>" +
+              word +
+              "<rt>" +
+              kanaToHira(token.reading) +
+              "</rt></ruby>"
+          : word
+      );
+      if (isKanji && options.addColor) {
+        $s.css("color", randomColor());
+      }
+      $element.append($s);
+    }
+  };
 })(jQuery);
 
 },{"kuromoji":18}]},{},[29]);
